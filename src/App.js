61cf1d68e75324f32e295329af662f039292b9ee
lwkchan <laurachan205@gmail.com>
2018-10-07 20:49:38 +0100

    feature: app successfully sends current location to api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+import { APP_ID } from './.open-weather-config'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const BASE_URI = 'https://api.openweathermap.org/data/2.5/weather?'
+const LATITUDE_PARAM = 'lat='
+const LONGTITUDE_PARAM = 'lon='
+const APP_ID_PARAM = 'appid='
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      apiUrl: ''
+    }
+  }
+
+  getApiUrl ({latitude, longitude}) {
+    return `${BASE_URI}${LATITUDE_PARAM}${latitude}&${LONGTITUDE_PARAM}${longitude}&${APP_ID_PARAM}${APP_ID}`
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      const apiUrl = this.getApiUrl(coords)
+      this.setState({ apiUrl })
+      fetch(apiUrl)
+        .then((response) => response.json() )
+        .then((data) => { console.log(data)})
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>{console.log(this.state)}</h1>
       </div>
     );
   }
